f1984a90e59b848d6bf5a445e8abf813301d2497
Filipe Aguilar <filipeaguilardeveloper@gmail.com>
2019-11-01 09:51:06 -0300

    Arquivo principal da aplicação, utilizada para executar todas as funcionalidades da mesma

---
+++ main.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+from GeradorPdf import GeradorPdf
+
+
+prepararDocumento = GeradorPdf(tipo_relatorio=1, titulo_relatorio=u"Apuração de Comissão",
+                            nome_empresa=u"RS Embreagem LTDA.", periodo_inicial=u"01/07/2019",
+                            periodo_final=u"31/07/2019")
+
+'''
+prepararDocumento = GeradorPdf(tipo_relatorio=2, titulo_relatorio=u"Relatório de comissão de mecânico",
+                            nome_empresa=u"QRLabel58", periodo_inicial=u"01/07/2019", periodo_final=u"31/07/2019",
+                            funcao=u"Ajudante de mecânico")
+
+
+prepararDocumento = GeradorPdf(tipo_relatorio=3, titulo_relatorio=u"Relatório de comissão telemarketing",
+                            nome_empresa=u"RS Embreagem LTDA.", periodo_inicial=u"01/07/2019",
+                            periodo_final=u"31/07/2019", turno=u"Tarde")
+
+
+prepararDocumento = GeradorPdf(tipo_relatorio=4, titulo_relatorio=u"Relatório de prêmio",
+                            nome_empresa=u"RS Embreagem LTDA.", periodo_inicial=u"01/07/2019",
+                            periodo_final=u"31/07/2019")
+
+
+prepararDocumento = GeradorRelatorioPdf(tipo_relatorio=5, titulo_relatorio=u"Relatório de comissão de vendedor",
+                            nome_empresa=u"RS Embreagem LTDA.", periodo_inicial=u"01/07/2019",
+                            periodo_final=u"31/07/2019")
+'''
+
+prepararDocumento.criarCabecalhoPdf()
+prepararDocumento.prepararDadosTabela()
+prepararDocumento.criarDocumentoPdf('modelo_exemplo')
